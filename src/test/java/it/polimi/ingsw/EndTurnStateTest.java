d9a1e12e182fa653233d0c5e45bad08a52db4bd7
niccoloborrelli <niccolo.borrelli@mail.polimi.it>
2020-06-28 14:25:50 +0200

    moved resource folder in right position

---
+++ src/test/java/it/polimi/ingsw/EndTurnStateTest.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EndTurnStateTest {
+
+    @Test
+    void onStateTransition() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(60101);
+        Socket socket = new Socket("localhost", 60101);
+        HandlerHub handlerHub = new HandlerHub();
+        Player player1 = new Player();
+        Controller controller1 = new Controller();
+        Player player2 = new Player();
+        Controller controller2 = new Controller();
+        handlerHub.getHandlerControllerHashMap().put(controller1, new Handler(socket, handlerHub));
+        handlerHub.getHandlerControllerHashMap().put(controller2, new Handler(socket, handlerHub));
+        controller1.setHandlerHub(handlerHub);
+        controller2.setHandlerHub(handlerHub);
+        player1.setController(controller1);
+        player2.setController(controller2);
+        controller1.setPlayer(player1);
+        controller2.setPlayer(player2);
+        TurnManager turnManager = new TurnManager();
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        turnManager.setPlayers(players);
+        player1.getStateManager().setTurnManager(turnManager);
+        player2.getStateManager().setTurnManager(turnManager);
+
+        State endTurn1 = new EndTurnState(player1);
+        State endTurn2 = new EndTurnState(player2);
+
+        player1.getActionsToPerform().clear();
+        player1.getStateManager().setCurrent_state(endTurn1);
+        endTurn1.onStateTransition();
+
+    }
+}
--- src/test/java/it/polimi/ingsw/LobbyManagerTest.java
+++ src/test/java/it/polimi/ingsw/LobbyManagerTest.java
@@ -2,7 +2,46 @@ package it.polimi.ingsw;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LobbyManagerTest {
+
+    @Test
+    void addPlayer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(60101);
+        Socket socket = new Socket("localhost", 60101);
+        HandlerHub globalHub = new HandlerHub();
+        Player player1 = new Player();
+        Controller controller1 = new Controller();
+        Player player2 = new Player();
+        Controller controller2 = new Controller();
+        Player player3 = new Player();
+        Controller controller3 = new Controller();
+        globalHub.addHandlerForSocket(socket, controller1);
+        globalHub.addHandlerForSocket(socket, controller2);
+        globalHub.addHandlerForSocket(socket, controller3);
+        player1.setController(controller1);
+        player2.setController(controller2);
+        player3.setController(controller3);
+        controller1.setPlayer(player1);
+        controller2.setPlayer(player2);
+        controller3.setPlayer(player3);
+
+        StateManager stateManager = new StateManager();
+        stateManager.createBaseStates(player1);
+        stateManager.createBaseStates(player2);
+        stateManager.createBaseStates(player3);
+        player1.setStateManager(stateManager);
+        player2.setStateManager(stateManager);
+        player3.setStateManager(stateManager);
+
+        LobbyManager lobbyManager = new LobbyManager(globalHub);
+        lobbyManager.addPlayer(player1, 2);
+        lobbyManager.addPlayer(player3, 3);
+        lobbyManager.addPlayer(player2, 2);
+    }
 }
