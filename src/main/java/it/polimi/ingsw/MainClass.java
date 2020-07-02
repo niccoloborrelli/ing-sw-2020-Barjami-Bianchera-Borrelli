63bfe9fe696175e2222786d5de31dacbb02dc141
Rei <10588572@polimi.it>
2020-07-02 13:22:39 +0200

    jar and mainclass

---
+++ src/main/java/it/polimi/ingsw/MainClass.java
@@ -0,0 +1,45 @@
+
+package it.polimi.ingsw;
+
+import java.io.IOException;
+
+public class MainClass {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if(args[0].equals("CLIClient")){
+            new Thread() {
+                @Override
+                public void run() {
+                    AAAClient CLIclient=new AAAClient();
+                    try {
+                        CLIclient.runCLI();
+                    } catch (IOException e) {
+                       // e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+
+        else if(args[0].equals("GUIClient")){
+            new Thread() {
+                @Override
+                public void run() {
+                    javafx.application.Application.launch(App.class);
+                }
+            }.start();
+        }
+
+        else if(args[0].equals("server")){
+            new Thread() {
+                @Override
+                public void run() {
+                    AAAServer server=new AAAServer();
+                    try {
+                        server.runServer();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+    }
+}
